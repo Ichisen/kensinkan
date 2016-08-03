79fab5257694133f666b51ca9ff6601e42062e42
Ichisen <alfironius@gmail.com>
2016-08-03 17:40:09 +0600

    подгрузка новостей и других данных

--- runtime.js
+++ runtime.js
@@ -2,12 +2,19 @@
  * Created by Ichisen-PC on 08.07.2016.
  */
 
-$( '.article-header' ).each(function(index, item) {
-    var $articleHeader = $(item);
-    var $articleBody = $articleHeader.parent().find('.article-body');
-    $articleBody.hide();
-    $articleHeader.click(function(){ $articleBody.toggle() })
-});
+var articleHeaderBind = function() {
+    $( '.article-header' ).each(function(index, item) {
+        var $articleHeader = $(item);
+        var bind = $articleHeader.attr('data-event-bind');
+
+        if(bind != 'true') {
+            var $articleBody = $articleHeader.parent().find('.article-body');
+            $articleBody.hide();
+            $articleHeader.click(function(){ $articleBody.toggle() });
+            $articleHeader.attr('data-event-bind',"true")
+        }
+    });
+};
 
 var currentURLHash;
 
@@ -22,3 +29,66 @@ var intervalID = setInterval(function(){
         currentURLHash = urlHash;
     }
 },50);
+
+var loadHTMLContent = function(url, callBack) {
+    $.ajax({
+        url: url +'/content.html',
+        method: 'GET',
+        complete: function(response) {
+            if(response.status != 200) return;
+
+            callBack( response.responseText );
+            articleHeaderBind();
+        }
+    })
+};
+
+var LOADCOUNT = 5;
+
+$( '.load-content' ).each(function(index, item) {
+    var $loadTarget = $(item);
+    var url = $(item).attr('data-url');
+    var isSingle = $(item).attr('data-single') == 'true';
+
+    if(isSingle){
+        loadHTMLContent(url, function(responseText){
+            $( responseText ).insertBefore( $loadTarget );
+            $loadTarget.remove()
+        })
+
+    } else {
+        $.ajax({
+            url: url +'/data.json',
+            method: 'GET',
+            complete: function(response) {
+                if(response.status != 200) return;
+
+                var data = JSON.parse(response.responseText);
+
+                var items = data.items || [];
+                var length = LOADCOUNT < items.length ? LOADCOUNT : items.length;
+                var lengthState = length;
+                var selfURL;
+
+                for(var i = 0; i < length; i++) {
+
+
+                    (function(item){
+                        selfURL = url + '/' + item.url;
+                        loadHTMLContent(selfURL, function(responseText){
+                            item.content = responseText;
+                            lengthState -= 1;
+
+                            if(lengthState == 0) {
+                                for(var j = 0; j < length; j++) {
+                                    $( items[j].content ).insertBefore( $loadTarget );
+                                }
+                                $loadTarget.remove();
+                            }
+                        })
+                    })(items[i]);
+                }
+            }
+        })
+    }
+});
